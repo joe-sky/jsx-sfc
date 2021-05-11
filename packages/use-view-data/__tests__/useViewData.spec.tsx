854a7d91ba9b00b763c0a777f6103c66375ac25d
joe-sky <hitomi_doax@sina.com>
2021-05-11 12:33:24 +0800

    test: :white_check_mark: update tests

--- packages/use-view-data/__tests__/useViewData.spec.tsx
+++ packages/use-view-data/__tests__/useViewData.spec.tsx
@@ -3,11 +3,22 @@ import { act } from 'react-dom/test-utils';
 import { shallow, mount } from 'enzyme';
 import { createUseViewData, ViewDataType } from '../src/index';
 
+type ViewData = ViewDataType<typeof useViewData>;
+
+type AddCountProps = { text: string } & Pick<ViewData, 'count' | 'setCount'>;
+
+const AddCount: React.FC<AddCountProps> = ({ text, count, setCount }) => {
+  return (
+    <div>
+      <i>{count}</i>
+      <button onClick={() => setCount(count + 1)}>{text}</button>
+    </div>
+  );
+};
+
 interface AppProps {
   title: string;
 }
-
-type Data = ViewDataType<typeof useViewData>;
 
 const useViewData = createUseViewData<AppProps>()(props => {
   const [count, setCount] = useState(0);
@@ -17,7 +28,7 @@ const useViewData = createUseViewData<AppProps>()(props => {
   }, []);
 
   return {
-    props,
+    ...props,
     count,
     setCount
   };
@@ -26,20 +37,16 @@ const useViewData = createUseViewData<AppProps>()(props => {
 const App: React.FC<AppProps> = props => {
   const data = useViewData(props);
 
-  return <AddCount parent={data} />;
-};
-
-const AddCount: React.FC<{ parent: Data }> = ({ parent }) => {
   return (
     <div>
-      <i>{parent.count}</i>
-      <button onClick={() => parent.setCount(parent.count + 1)}>{parent.props.title}</button>
+      <h1>{props.title}</h1>
+      <AddCount text="Add" {...data} />
     </div>
   );
 };
 
 test('rendering child component by viewData', async () => {
-  const wrapper = mount(<App title="Add" />);
+  const wrapper = mount(<App title="test" />);
   expect(wrapper.find('button').text()).toEqual('Add');
 
   await act(async () => {
@@ -50,4 +57,29 @@ test('rendering child component by viewData', async () => {
   });
   wrapper.update();
   expect(wrapper.find('i').text()).toEqual('1');
+});
+
+const TestAddCount: React.FC<{ text: string }> = ({ text }) => {
+  const [count, setCount] = useState(1);
+
+  return (
+    <div>
+      <b>{count}</b>
+      <AddCount text={text} count={count} setCount={setCount} />
+    </div>
+  );
+};
+
+test('independent test child component', async () => {
+  const wrapper = mount(<TestAddCount text="Delete" />);
+  expect(wrapper.find('button').text()).toEqual('Delete');
+
+  await act(async () => {
+    wrapper
+      .find('button')
+      .at(0)
+      .simulate('click');
+  });
+  wrapper.update();
+  expect(wrapper.find('b').text()).toEqual('2');
 });
